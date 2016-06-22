47afb6d8edfae6d2e05123975cfee9046226605d
wanaaaa <wanaaaa@gmail.com>
2016-06-21 20:33:15 -0400

    asdf

---
+++ src/Car.java
@@ -0,0 +1,5 @@
+/**
+ * Created by wanmac on 6/21/16.
+ */
+public class Car {
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by wanmac on 6/21/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int returned = returnSum(2,4);
+        System.out.println(returned);
+        int returned = returnSum(10,52);
+        System.out.println(returned);
+    }
+}
